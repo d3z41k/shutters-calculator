71fa228e0e7695d1af3a15f7648311a270e35961
d3z41k <d3z41k@gmail.com>
2021-10-18 23:44:59 +0200

    Add graceful shutdown

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"github.com/d3z41k/shutters-calculator/pkg/models/mysql"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -50,9 +53,24 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	if err := srv.ListenAndServe(); err != nil {
-		errorLog.Fatal(err)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			errorLog.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
 	}
+
+	log.Println("Server exiting")
 }
 
 func openDB(dsn string) (*sql.DB, error) {
